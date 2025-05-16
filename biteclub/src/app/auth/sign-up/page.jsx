47a45a6144fa5a099459b1e41fa4bb3dc90b44a9
n106ky (N.C) <n106ky@gmail.com>
2025-05-16 13:14:28 -0400

    UPDATE: Sign Up UI

--- biteclub/src/app/auth/sign-up/page.jsx
+++ biteclub/src/app/auth/sign-up/page.jsx
@@ -1,10 +1,15 @@
-import { SignUpForm } from '@/components/Sign-up-form'
+import { SignUpForm } from '@/components/Sign-up-form';
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <SignUpForm />
+    <div className="grid min-h-svh lg:grid-cols-2 pink-test">
+      <div className="relative hidden bg-brand-green lg:block"></div>
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <SignUpForm />
+          </div>
+        </div>
       </div>
     </div>
   );
