a520bd6d62e3db430064535221e2b445ced9d8f6
n106ky (N.C) <n106ky@gmail.com>
2025-05-17 10:46:00 -0400

    UPDATE: (bug fix) app routes, ADD: (auth-error page) back to login button

--- biteclub/src/app/(auth)/callback/route.js
+++ biteclub/src/app/(auth)/callback/route.js
@@ -6,14 +6,14 @@ export async function GET(request) {
   const code = searchParams.get('code');
 
   if (!code) {
-    return NextResponse.redirect(`${origin}/error`); // /auth/
+    return NextResponse.redirect(`${origin}/auth-error`);
   }
 
   const supabase = await createClient();
   const { error: sessionError } = await supabase.auth.exchangeCodeForSession(code);
 
   if (sessionError) {
-    return NextResponse.redirect(`${origin}/error`);
+    return NextResponse.redirect(`${origin}/auth-error`);
   }
 
   const {
@@ -22,7 +22,7 @@ export async function GET(request) {
   } = await supabase.auth.getUser();
 
   if (userError || !user) {
-    return NextResponse.redirect(`${origin}/error`);
+    return NextResponse.redirect(`${origin}/auth-error`);
   }
 
   // Check if user has already onboarded
--- biteclub/src/app/(auth)/confirm/route.js
+++ biteclub/src/app/(auth)/confirm/route.js
@@ -31,6 +31,6 @@ export async function GET(request) {
   }
 
   // return the user to an error page with some instructions
-  redirectTo.pathname = '/error'
+  redirectTo.pathname = '/auth-error'
   return NextResponse.redirect(redirectTo)
 }
