2e222a4fe6c942ec2799d3a79dc6e738245a8bd7
n106ky (N.C) <n106ky@gmail.com>
2025-06-04 16:12:20 -0400

    ADD: button on await page

--- biteclub/src/app/(main)/users/business/awaiting-verification/page.jsx
+++ biteclub/src/app/(main)/users/business/awaiting-verification/page.jsx
@@ -1,21 +1,26 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/auth/ui/Card';
+'use client';
 
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/auth/ui/Card';
+import { Button } from '@/components/shared/Button';
+import { useRouter } from 'next/navigation';
 export default function Page() {
+  const router = useRouter();
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <div className="flex flex-col gap-6">
           <Card>
             <CardHeader>
-              <CardTitle className="text-2xl">
-                Thank you for submitting your information!
-              </CardTitle>
+              <CardTitle className="text-2xl">Thank you for submitting your information!</CardTitle>
             </CardHeader>
             <CardContent>
               <p className="text-sm text-muted-foreground">
-                We’ve received your business license and details. Once we complete the verification process,
-                we’ll notify you by email.
+                We’ve received your business license and details. Once we complete the verification process, we’ll
+                notify you by email.
               </p>
+              <Button className="w-full font-roboto font-normal mt-4" onClick={() => router.push('/users/business')}>
+                Go to Dashboard
+              </Button>
             </CardContent>
           </Card>
         </div>
