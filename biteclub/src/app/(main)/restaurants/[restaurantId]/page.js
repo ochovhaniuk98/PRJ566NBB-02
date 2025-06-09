729bce944374046793109416a6642cb897471630
Cesca <32521038+cescaa@users.noreply.github.com>
2025-06-09 17:03:54 -0400

    corrected isOwner status for restaurant profile
    (my mistake!)

--- biteclub/src/app/(main)/restaurants/[restaurantId]/page.js
+++ biteclub/src/app/(main)/restaurants/[restaurantId]/page.js
@@ -6,5 +6,5 @@ import { useParams } from 'next/navigation';
 export default function RestaurantPage() {
   const { restaurantId } = useParams();
 
-  return <RestaurantProfile restaurantId={restaurantId} isOwner={true} />; // REMINDER TO CESCA! REMVOE TRUE
+  return <RestaurantProfile restaurantId={restaurantId} />;
 }
