d04a435dfbc6c15eea5957aaaadc391492d285e6
Olha Chovhaniuk <ochovhaniuk@gmail.com>
2025-07-11 12:56:51 -0400

    Saving and displaying nested replies

--- biteclub/src/components/shared/CommentThread.jsx
+++ biteclub/src/components/shared/CommentThread.jsx
@@ -4,19 +4,21 @@ import { Button } from './Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbsUp, faThumbsDown, faComment } from '@fortawesome/free-solid-svg-icons';
 import Image from 'next/image';
-import { fakeUser } from '@/app/data/fakeData';
 import { createClient } from '@/lib/auth/client';
-import { getGeneralUserMongoIDbySupabaseId, getGeneralUserProfileByMongoId } from '@/lib/db/dbOperations';
 
 //////////////// COMMENT THREAD FOR *** BLOG POST *** ONLY! ///////////////
 
 // *** Entire comment thread / container ***
 export default function CommentThread({ post }) {
+  // post and user
   const [blogPost, setBlogPost] = useState(null);
   const [user, setUser] = useState(null);
-  const [currentComments, setCurrentComments] = useState(null);
+
+  // comments
+  const [comments, setComments] = useState([]);
   const [commentsCount, setCommentsCount] = useState(0);
   const [fetchedComments, setFetchedComments] = useState(false);
+  const [newComment, setNewComment] = useState('');
 
   // set current blog post and user
   useEffect(() => {
@@ -47,39 +49,61 @@ export default function CommentThread({ post }) {
     fetchUser();
   }, [post]);
 
-  // all comments (first comment is fake by default)
-  const [comments, setComments] = useState([]);
+  // nest replies within parent comments (parent comment -> [reply 1, reply 2 ...])
+  function nestComments(comments) {
+    const commentMap = {};
+    const nestedComments = [];
 
-  // get all comments for current blog posts
+    comments.forEach(comment => {
+      comment.replies = [];
+      commentMap[comment._id] = comment;
+    });
+
+    comments.forEach(comment => {
+      if (comment.parent_id) {
+        const parent = commentMap[comment.parent_id];
+        if (parent) {
+          parent.replies.push(comment);
+        }
+      } else {
+        nestedComments.push(comment);
+      }
+    });
+
+    console.log('nestedComments: ', nestedComments);
+
+    return nestedComments;
+  }
+
+  // get all comments for a current blog posts
+  const fetchComments = async () => {
+    try {
+      const res = await fetch(`/api/blog-posts/comments/get-comments-by-post-id?postId=${blogPost._id}`);
+      if (!res.ok) throw new Error('Failed to fetch comments');
+
+      const data = await res.json();
+
+      setComments(nestComments(data));
+      setCommentsCount(data.length);
+
+      setFetchedComments(true);
+
+      console.log('Comments length: ', data.length);
+    } catch (err) {
+      console.error('Error fetching comments:', err);
+    }
+  };
+
   useEffect(() => {
     if (!blogPost) return;
 
-    const fetchComments = async () => {
-      try {
-        const res = await fetch(`/api/blog-posts/comments/get-comments-by-post-id?postId=${blogPost._id}`);
-        if (!res.ok) throw new Error('Failed to fetch comments');
-
-        const data = await res.json();
-
-        setComments(data);
-        setCommentsCount(data.length);
-        setFetchedComments(true);
-      } catch (err) {
-        console.error('Error fetching comments:', err);
-      }
-    };
-
     fetchComments();
-  }, [blogPost, comments]);
+  }, [blogPost]);
 
-  // TODO: add new comments
-  // TODO: add new replies
-
-  // add a single comment to all comments array
-  const [newComment, setNewComment] = useState('');
+  // add new comment
   const addComment = async () => {
     if (newComment.trim()) {
-      // TODO: add new comment
+      // add new comment
       try {
         if (!blogPost || !user) return;
 
@@ -105,7 +129,7 @@ export default function CommentThread({ post }) {
         const comment = data.comment;
 
         setComments(prev => [...prev, comment]);
-        setCommentsCount(prev => [prev + 1]);
+        setCommentsCount(prev => prev + 1);
 
         console.log('Posted comment:', data);
       } catch (err) {
@@ -117,31 +141,68 @@ export default function CommentThread({ post }) {
   };
 
   // add reply from profile owner to a comment
-  const addReply = (parentId, replyText) => {
-    const insertReply = items =>
-      items.map(item => {
-        if (item.id === parentId) {
-          return {
-            ...item,
-            replies: [
-              ...item.replies,
-              {
-                id: Date.now(),
-                author: 'ProfileOwnerName',
-                avatarURL: 'https://i.pravatar.cc/150?img=10',
-                content: replyText,
-                timestamp: new Date(),
-                replies: [],
-              },
-            ],
-          };
-        } else if (item.replies?.length > 0) {
-          return { ...item, replies: insertReply(item.replies) };
-        }
-        return item;
-      });
+  const addReply = async (parentId, replyContent, setReplyContent) => {
+    // add new reply
 
-    setComments(insertReply(comments));
+    if (replyContent.trim()) {
+      // add new reply
+      try {
+        if (!blogPost || !user) return;
+
+        // post request to create a new comment
+        const res = await fetch('/api/blog-posts/comments/add-comment', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            blogPostId: blogPost._id,
+            parent_id: parentId,
+            avatarURL: user.userProfilePicture.url,
+            content: replyContent,
+            author: user.username,
+            date_posted: new Date().toISOString(),
+            user: user._id,
+          }),
+        });
+
+        if (!res.ok) throw new Error(`Could not create new reply ${res.status}`);
+        const data = await res.json();
+        const comment = data.comment;
+
+        fetchComments(); // update the list of comments/replies
+        setReplyContent('');
+
+        console.log('Posted reply:', data);
+      } catch (err) {
+        console.error('Error posting reply:', err);
+      }
+    }
+
+    // const insertReply = items =>
+    //   items.map(item => {
+    //     if (item.id === parentId) {
+    //       return {
+    //         ...item,
+    //         replies: [
+    //           ...item.replies,
+    //           {
+    //             id: Date.now(),
+    //             author: 'ProfileOwnerName',
+    //             avatarURL: 'https://i.pravatar.cc/150?img=10',
+    //             content: replyText,
+    //             timestamp: new Date(),
+    //             replies: [],
+    //           },
+    //         ],
+    //       };
+    //     } else if (item.replies?.length > 0) {
+    //       return { ...item, replies: insertReply(item.replies) };
+    //     }
+    //     return item;
+    //   });
+
+    // setComments(insertReply(comments));
   };
 
   return (
@@ -173,68 +234,82 @@ export default function CommentThread({ post }) {
 // *** single comment with engagement icons + input field for replying ***
 const Comment = ({ comment, onReply }) => {
   const [showReplyInput, setShowReplyInput] = useState(false);
+  const [showReplyBtn, setShowReplyBtn] = useState(false);
   const [replyContent, setReplyContent] = useState('');
+
   const [likes, setLikes] = useState(0);
   const [dislikes, setDislikes] = useState(0);
 
+  // allow only one level nested comments
+  useEffect(() => {
+    if (comment?.parent_id === null) {
+      setShowReplyBtn(true);
+    } else {
+      setShowReplyBtn(false);
+    }
+  }, []);
+
   const handleReply = () => {
     if (replyContent.trim()) {
-      onReply(comment.id, replyContent);
-      setReplyContent('');
-      setShowReplyInput(false);
+      onReply(comment._id, replyContent, setReplyContent);
     }
   };
 
   return (
-    <div className="flex mb-6">
-      <div className="relative w-10 h-10 rounded-full mr-3">
-        <Image src={comment.avatarURL} alt={comment.author} fill={true} className="rounded-full object-cover w-full" />
-      </div>
-      <div className="flex-1">
-        <div className="text-sm font-semibold">
-          {comment.author} <span className="text-gray-500 text-xs">{formatTimeAgo(comment.date_posted)}</span>
+    <>
+      <div className="flex mb-6">
+        <div className="relative w-10 h-10 rounded-full mr-3">
+          <Image
+            src={comment.avatarURL}
+            alt={comment.author}
+            fill={true}
+            className="rounded-full object-cover w-full"
+          />
         </div>
-        <div className="mt-1 mb-2">{comment.content}</div>
-
-        {/* like, dislike, reply */}
-        <div className="flex gap-4 text-gray-500 text-sm">
-          <button onClick={() => setLikes(likes + 1)} className="hover:text-brand-navy cursor-pointer">
-            <FontAwesomeIcon icon={faThumbsUp} className={`icon-md text-brand-navy`} /> {likes}
-          </button>
-          <button onClick={() => setDislikes(dislikes + 1)} className="hover:text-brand-navy cursor-pointer">
-            <FontAwesomeIcon icon={faThumbsDown} className={`icon-md text-brand-navy`} />
-          </button>
-          <button onClick={() => setShowReplyInput(!showReplyInput)} className="hover:text-black cursor-pointer">
-            <FontAwesomeIcon icon={faComment} className={`icon-md text-brand-navy`} /> Reply
-          </button>
-        </div>
-
-        {/* reply input field + button */}
-        {showReplyInput && (
-          <div className="mt-2">
-            <textarea
-              rows={2}
-              placeholder="Write a reply..."
-              value={replyContent}
-              onChange={e => setReplyContent(e.target.value)}
-              className="w-full p-2"
-            />
-            <Button type="submit" variant="secondary" className="w-20 ml-auto" onClick={handleReply}>
-              Reply
-            </Button>
+        <div className="flex-1">
+          <div className="text-sm font-semibold">
+            {comment.author} <span className="text-gray-500 text-xs">{formatTimeAgo(comment.date_posted)}</span>
           </div>
-        )}
+          <div className="mt-1 mb-2">{comment.content}</div>
 
-        {/* vertical trail of replies */}
-        {comment.replies?.length > 0 && (
+          {/* like, dislike, reply */}
+          <div className="flex gap-4 text-gray-500 text-sm">
+            <button onClick={() => setLikes(likes + 1)} className="hover:text-brand-navy cursor-pointer">
+              <FontAwesomeIcon icon={faThumbsUp} className={`icon-md text-brand-navy`} /> {likes}
+            </button>
+            <button onClick={() => setDislikes(dislikes + 1)} className="hover:text-brand-navy cursor-pointer">
+              <FontAwesomeIcon icon={faThumbsDown} className={`icon-md text-brand-navy`} />
+            </button>
+            {showReplyBtn && (
+              <button onClick={() => setShowReplyInput(!showReplyInput)} className="hover:text-black cursor-pointer">
+                <FontAwesomeIcon icon={faComment} className={`icon-md text-brand-navy`} /> Reply
+              </button>
+            )}
+          </div>
+
+          {/* reply input field + button */}
+          {showReplyInput && (
+            <div className="mt-2">
+              <textarea
+                rows={2}
+                placeholder="Write a reply..."
+                value={replyContent}
+                onChange={e => setReplyContent(e.target.value)}
+                className="w-full p-2"
+              />
+              <Button type="submit" variant="secondary" className="w-20 ml-auto" onClick={handleReply}>
+                Reply
+              </Button>
+            </div>
+          )}
+          {/* vertical trail of replies */}
           <div className="mt-4 pl-4 border-l-2 border-brand-peach">
-            {comment.replies.map(reply => (
-              <Comment key={reply.id} comment={reply} onReply={onReply} />
-            ))}
+            {comment?.replies &&
+              comment.replies.map(reply => <Comment key={reply._id} comment={reply} onReply={onReply} />)}
           </div>
-        )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
