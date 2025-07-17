5ce37f6789f469fa3d44631e50e57096fde9fda3
Jashanpreet Singh <jashan20preet21@gmail.com>
2025-07-17 12:04:16 -0400

    Reverse previous accident

--- biteclub/src/app/(main)/blog-posts/page.jsx
+++ biteclub/src/app/(main)/blog-posts/page.jsx
@@ -1,97 +1,10 @@
-'use client';
-import { useEffect, useState, useRef } from 'react';
-import Masonry from 'react-masonry-css';
-import BlogPostCard from '../shared/BlogPostCard';
-import { Button } from '../shared/Button';
+import { Suspense } from 'react';
+import BlogPostsList from '@/components/blogPosts/BlogPostsList';
 
-export default function ExploringBlogPosts() {
-  const [blogPosts, setBlogPosts] = useState([]);
-  const [page, setPage] = useState(1);
-
-  const [hasMore, setHasMore] = useState(true);
-  const [fetchCompleted, setFetchCompleted] = useState(false);
-
-  const scrollPositionRef = useRef(0);
-
-  // for blog posts' Masonry grid
-  const breakpointColumnsObj = {
-    default: 3,
-    1024: 2,
-    640: 1,
-  };
-
-  // fetch Exploring blog posts
-  const fetchExploringPosts = async (reset = false) => {
-    setFetchCompleted(false);
-
-    try {
-      const res = await fetch(`/api/blog-posts/get-exploring-posts?page=${page}&limit=20`);
-      const data = await res.json();
-
-      if (reset) {
-        setPage(1);
-        setBlogPosts(data.posts);
-      } else {
-        // append data to existing list
-        setBlogPosts(prev => {
-          const ids = new Set(prev.map(post => post._id));
-          const newOnes = data.posts.filter(post => !ids.has(post._id)); // avoid duplicates
-          return [...prev, ...newOnes];
-        });
-      }
-
-      // if we've fetched everything, stop loading more
-      if ((reset ? data?.posts?.length : blogPosts?.length + data?.posts?.length) >= data?.totalCount) {
-        setHasMore(false);
-      } else {
-        setHasMore(true);
-      }
-      setFetchCompleted(true);
-    } catch (error) {
-      setFetchCompleted(true);
-      console.error('Failed to fetch exploring blog posts:', error);
-    }
-  };
-
-  // page = 1
-  useEffect(() => {
-    fetchExploringPosts(true); // reset = true
-  }, []);
-
-  // when page > 1
-  useEffect(() => {
-    fetchExploringPosts();
-  }, [page]);
-
-  useEffect(() => {
-    if (page > 1 && fetchCompleted) {
-      // restore exact user's scroll position
-      window.scrollTo({ top: scrollPositionRef.current, behavior: 'auto' });
-    }
-  }, [blogPosts]);
-
-  const loadMore = () => {
-    // save current vertical scroll position before state update
-    scrollPositionRef.current = window.scrollY;
-    setPage(prev => prev + 1);
-  };
-
+export default function BlogPostsResults() {
   return (
-    <>
-      {fetchCompleted && (
-        <>
-          <Masonry breakpointCols={breakpointColumnsObj} className="flex gap-2 mt-4" columnClassName="space-y-2">
-            {blogPosts.map((post, i) => (
-              <BlogPostCard key={post._id || i} blogPostData={post} />
-            ))}
-          </Masonry>
-          {hasMore && (
-            <div className="mt-6 flex justify-center">
-              <Button onClick={loadMore}>Load More</Button>
-            </div>
-          )}
-        </>
-      )}
-    </>
+    <Suspense fallback={<div>Loading Blog Posts results…</div>}>
+      <BlogPostsList />
+    </Suspense>
   );
 }
