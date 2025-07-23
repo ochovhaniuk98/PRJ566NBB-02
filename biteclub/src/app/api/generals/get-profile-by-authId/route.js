e7d415c9ffe9c68fa5854efd737ed5a133b95772
n106ky (N.C) <n106ky@gmail.com>
2025-07-22 21:56:35 -0400

    minor update: comments for API route

--- biteclub/src/app/api/generals/get-profile-by-authId/route.js
+++ biteclub/src/app/api/generals/get-profile-by-authId/route.js
@@ -17,6 +17,10 @@ export async function GET(req) {
     }
 
     return NextResponse.json({ profile }, { status: 200 });
+    // [!] Important: When calling this API and using res.json(),
+    // make sure to destructure using: const { profile } = await res.json();
+    // This matches the returned shape: { profile }
+    
   } catch (err) {
     console.error('Error fetching user profile:', err);
     return NextResponse.json({ message: 'Server error' }, { status: 500 });
