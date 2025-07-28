bbc568e6e703f150216394ab3093d09bebc4c38d
Cesca <32521038+cescaa@users.noreply.github.com>
2025-07-28 14:43:01 -0400

    replaced logo image in search bar

--- biteclub/src/components/shared/SearchBar.jsx
+++ biteclub/src/components/shared/SearchBar.jsx
@@ -18,8 +18,8 @@ export default function SearchBar() {
 
   return (
     <div className="fixed w-full flex justify-between z-40 bg-white/50 main-side-padding ">
-      <div className="relative  h-8 size-auto w-60 mt-3">
-        <Image src="/img/logo.jpg" alt="logo pic" className="object-contain rounded-br-md" fill={true} />
+      <div className="relative  h-12 size-auto w-60 mt-1">
+        <Image src="/img/logo_final.png" alt="logo pic" className="object-contain rounded-br-md" fill={true} />
       </div>
       <div className="relative w-128 max-w-full">
         <span className="absolute inset-y-0 left-3 flex items-center text-brand-grey">
