afeaa91458806adaac1519e41d57bbce005a8563
Cesca <32521038+cescaa@users.noreply.github.com>
2025-08-06 01:08:56 -0400

    Updated CompletedChallengeModal UI

--- biteclub/src/components/challenges/ChallengeCompletedModal.jsx
+++ biteclub/src/components/challenges/ChallengeCompletedModal.jsx
@@ -5,37 +5,55 @@ import Image from 'next/image';
 // modal that pops up automatically when user completes WHOLE challenge
 export default function ChallengeCompletedModal({ numPointsWon = 123, setShowChallengeCompletedModal }) {
   return (
-    <div className="fixed inset-0 z-100 w-screen h-screen bg-brand-navy flex flex-col items-center gap-y-8">
-      {/* Close Modal Icon */}
-      <div className="fixed top-3 right-3 p-1 w-fit h-fit" onClick={() => setShowChallengeCompletedModal(false)}>
-        <FontAwesomeIcon
-          icon={faXmarkCircle}
-          className="text-4xl text-brand-blue cursor-pointer transition-transform duration-200 hover:scale-125"
-        />
-      </div>
-      {/* Num of Points Header */}
-      <div className="text-white font-primary text-3xl flex items-baseline-last gap-x-1">
-        <div className="flex items-center gap-x-2">
-          <FontAwesomeIcon icon={faPlus} className="text-5xl text-white cursor-pointer" />
-          <div className=" relative w-56 h-32">
-            <div className="absolute top-0 right-1 font-secondary text-[8rem] ">{numPointsWon}</div>
+    <div className="fixed inset-0 z-100 w-screen h-screen bg-brand-green flex items-center justify-between">
+      <div
+        className="h-full w-1/4 bg-cover"
+        style={{
+          backgroundImage: "url('/img/confetti-large.png')",
+          backgroundSize: '290%',
+          backgroundPosition: '0rem',
+          transform: 'scaleX(-1)',
+        }}
+      ></div>
+
+      <div className="w-2/4 h-full flex flex-col items-center justify-evenly text-brand-navy">
+        {/* Num of Points Header */}
+        <div className="text-brand-navy font-primary text-3xl flex items-baseline-last gap-x-0">
+          <div className="flex items-center gap-x-1">
+            <FontAwesomeIcon icon={faPlus} className="text-6xl text-brand-navy cursor-pointer" />
             <div
-              className="absolute top-1 right-0 font-secondary text-[8rem] text-brand-green"
+              className="font-secondary text-[12rem] text-brand-navy"
               style={{
-                WebkitTextStroke: '1px white',
+                WebkitTextStroke: '2px white',
               }}
             >
               {numPointsWon}
             </div>
           </div>
+          <span className="text-3xl font-semibold">points</span>
         </div>
-        <span className="text-3xl font-semibold">points</span>
+        {/* Coin Image */}
+        <div className="relative h-86 w-full">
+          <Image src={'/img/coinWithFork.png'} alt={'coin and confetti'} className="object-contain" fill={true} />
+        </div>
+        <span className="text-brand-navy text-3xl font-semibold font-primary uppercase">Challenge Completed!</span>
       </div>
-      {/* Coin + Confetti Image */}
-      <div className="relative h-110 w-full">
-        <Image src={'/img/coinAndConfetti.png'} alt={'coin and confetti'} className="object-contain" fill={true} />
+      <div
+        className="h-full w-1/4 bg-cover"
+        style={{
+          backgroundImage: "url('/img/confetti-large.png')",
+          backgroundSize: '290%',
+          backgroundPosition: '0rem',
+        }}
+      >
+        {/* Close Modal Icon */}
+        <div className="fixed top-3 right-3 p-1 w-fit h-fit" onClick={() => setShowChallengeCompletedModal(false)}>
+          <FontAwesomeIcon
+            icon={faXmarkCircle}
+            className="text-5xl text-white cursor-pointer transition-transform duration-200 hover:scale-125"
+          />
+        </div>
       </div>
-      <h2 className="text-brand-aqua-lite">Challenge Completed!</h2>
     </div>
   );
 }
