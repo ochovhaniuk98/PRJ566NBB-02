955e305f2187fa04d135e0cb91a9ef2820a7c337
Jashanpreet Singh <jashan20preet21@gmail.com>
2025-08-07 13:28:36 -0400

    Redeem coupon route

---
+++ biteclub/src/app/api/redeemCoupon/route.js
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server';
+import { getGeneralUserProfileBySupabaseId, redeemPoints, updatePoints } from '@/lib/db/dbOperations';
+import { User } from '@/lib/model/dbSchema';
+
+export async function POST(req) {
+  try {
+    const body = await req.json();
+
+    const {
+      couponCode
+    } = body;
+
+    if (!couponCode) {
+      return NextResponse.json({ message: "Please provide a coupon code" }, { status: 400 })
+    }
+    let user = await User.findOne({ "coupon.code": couponCode });
+    if (user == null) {
+      return NextResponse.json({ message: "Coupon not found" }, { status: 404 })
+    }
+
+    const value = user.coupon.value;
+    user = await User.findOneAndUpdate({ "coupon.code": couponCode }, { "coupon": null }, { new: true });
+    console.log(user);
+    if (user != null) {
+      return NextResponse.json({ message: "Successfully redeemed", value }, { status: 200 });
+    } else {
+      return NextResponse.json({ message: "Coupon not found" }, { status: 404 })
+    }
+
+  } catch (err) {
+    console.error('Error redeeming points:', err);
+    return NextResponse.json({ message: 'Server error' }, { status: 500 });
+  }
+}
