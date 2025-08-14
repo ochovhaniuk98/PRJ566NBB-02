7377e8375636f36c6da0d42f3d37ee4bb3d0e6e0
IRISH BANGA <122631145+IrishBanga@users.noreply.github.com>
2025-08-14 10:45:30 -0400

    refactor POST endpoint to simplify reward logic and use mongoId for redeeming points

--- biteclub/src/app/api/redeem/route.js
+++ biteclub/src/app/api/redeem/route.js
@@ -1,43 +1,22 @@
 import { NextResponse } from 'next/server';
-import { getGeneralUserProfileBySupabaseId, redeemPoints } from '@/lib/db/dbOperations';
+import { redeemPoints } from '@/lib/db/dbOperations';
 
 export async function POST(req) {
   try {
     const body = await req.json();
+    const { mongoId, reward, pointsNeeded } = body;
 
-    const { supabaseId, reward } = body;
-
-    let pointsNeeded = null;
-    if (reward == 5) {
-      pointsNeeded = 1000;
-    } else if (reward == 10) {
-      pointsNeeded = 1500;
-    } else if (reward == 15) {
-      pointsNeeded = 2000;
-    } else if (reward == 20) {
-      pointsNeeded = 2500;
-    } else if (reward == 25) {
-      pointsNeeded = 1000;
-    }
-
-    // if (!supabaseId || !username) {
-    //   return NextResponse.json({ message: 'Missing required fields' }, { status: 400 });
-    // }
-    const profile = await getGeneralUserProfileBySupabaseId({ supabaseId });
-    console.log('Profile', profile);
-    if (profile.numOfPoints < pointsNeeded) {
-      return NextResponse.json({ message: 'Insufficient Points' }, { status: 409 });
-    }
-
-    const couponCode = await redeemPoints(supabaseId, reward);
+    const couponCode = await redeemPoints(mongoId, reward, pointsNeeded);
     console.log('Coupon Code received: ', couponCode);
+
     if (couponCode) {
       return NextResponse.json({ message: 'Points successfully redeemed!', couponCode }, { status: 200 });
-    } else {
-      return NextResponse.json({ message: 'Insufficient Points/Coupon already exists' }, { status: 409 });
     }
   } catch (err) {
     console.error('Error redeeming points:', err);
+    if (err.message === 'Insufficient Points' || err.message === 'Coupon already exists') {
+      return NextResponse.json({ message: err.message }, { status: 409 });
+    }
     return NextResponse.json({ message: 'Server error' }, { status: 500 });
   }
 }
