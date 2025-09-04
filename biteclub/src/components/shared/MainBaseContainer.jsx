c7321d47311da7bf9755e42410d8035493ee2d07
Cesca <32521038+cescaa@users.noreply.github.com>
2025-09-03 23:50:20 -0400

    Adjusted global X-axis padding + spacing for mobile

--- biteclub/src/components/shared/MainBaseContainer.jsx
+++ biteclub/src/components/shared/MainBaseContainer.jsx
@@ -12,7 +12,7 @@ export default function MainBaseContainer({ children }) {
           <div className="flex flex-col items-center">{children}</div>
         </div>
       ) : (
-        <div className="absolute top-0 left-12 right-0">
+        <div className="absolute top-0 md:left-12 right-0 px-0">
           <div className="flex flex-col items-center">{children}</div>
         </div>
       )}
