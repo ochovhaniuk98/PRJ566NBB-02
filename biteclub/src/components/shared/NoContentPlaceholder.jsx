c3dece8d6ac3367e09e7e75302987e7981a8c030
Cesca <32521038+cescaa@users.noreply.github.com>
2025-09-24 14:52:11 -0400

    Added responsive styles for restaurant profile 'edit' sub-forms

--- biteclub/src/components/shared/NoContentPlaceholder.jsx
+++ biteclub/src/components/shared/NoContentPlaceholder.jsx
@@ -1,7 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenClip, faHeart, faUsers, faImage, faFaceFrown } from '@fortawesome/free-solid-svg-icons';
 
-export default function NoContentPlaceholder({ contentType = 'content', iconImgNum = 1, forSearchResults = false }) {
+export default function NoContentPlaceholder({
+  contentType = 'content',
+  iconImgNum = 1,
+  forSearchResults = false,
+  className = '',
+}) {
   let icon;
 
   switch (iconImgNum) {
@@ -24,7 +29,9 @@ export default function NoContentPlaceholder({ contentType = 'content', iconImgN
       icon = faPenClip;
   }
   return (
-    <div className="flex flex-col gap-4 items-center justify-center w-sm border-2 border-white py-16 px-8 m-auto rounded-2xl text-brand-grey bg-brand-blue-lite/30 text-center min-h-60">
+    <div
+      className={`flex flex-col gap-4 items-center justify-center md:w-sm w-xs border-2 border-white md:py-16 py-12 md:px-8 px-4 m-auto rounded-2xl text-brand-grey bg-brand-blue-lite/30 text-center min-h-60 ${className}`}
+    >
       <FontAwesomeIcon icon={icon} className={`text-4xl text-brand-blue`} />
       <h2 className="normal-case">
         {forSearchResults ? (
